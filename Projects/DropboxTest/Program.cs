d7945d3ecf83f4a223c9e19c33e20c482bfffe91
yariki <iyariki.ya@gmail.com>
2019-12-24 23:43:29 +0200

    new projects

---
+++ Projects/DropboxTest/Program.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using Dropbox.Api;
+using Dropbox.Api.Files;
+
+namespace DropboxTest
+{
+    class Program
+    {
+        // Add an ApiKey (from https://www.dropbox.com/developers/apps) here
+        private const string ApiKey = "jiaz0yjc8015g6t";
+
+        // This loopback host is for demo purpose. If this port is not
+        // available on your machine you need to update this URL with an unused port.
+        private const string LoopbackHost = "http://127.0.0.1:52475/";
+
+        // URL to receive OAuth 2 redirect from Dropbox server.
+        // You also need to register this redirect URL on https://www.dropbox.com/developers/apps.
+        private readonly Uri RedirectUri = new Uri(LoopbackHost + "authorize");
+
+        // URL to receive access token from JS.
+        private readonly Uri JSRedirectUri = new Uri(LoopbackHost + "token");
+
+
+        [DllImport("kernel32.dll", ExactSpelling = true)]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        private static extern bool SetForegroundWindow(IntPtr hWnd);
+
+        [STAThread]
+        static int Main(string[] args)
+        {
+            var instance = new Program();
+
+            var task = Task.Run(instance.Run);
+            task.Wait();
+
+            return task.Result;
+
+        }
+
+        private async Task<int> Run()
+        {
+            DropboxCertHelper.InitializeCertPinning();
+
+            var accessToken = await this.GetAccessToken();
+            if (string.IsNullOrEmpty(accessToken))
+            {
+                return 1;
+            }
+
+            using (var client = new DropboxClient(accessToken))
+            {
+
+                var list = await client.Files.ListFolderAsync(String.Empty);
+
+                foreach (var item in list.Entries)
+                {
+                    Console.WriteLine($"Item: {item} - IsFile: {item.IsFile} - IsFolder: {item.IsFolder}");
+                }
+
+                try
+                {
+                
+                    var folderArg = new CreateFolderArg("/temp");
+                    var folder = await client.Files.CreateFolderV2Async(folderArg);
+                    Console.WriteLine($"Folder created: {folder.Metadata.Name}");
+
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e);
+                }
+            }
+
+            return 0;
+        }
+
+
+        private async Task HandleOAuth2Redirect(HttpListener http)
+        {
+            var context = await http.GetContextAsync();
+
+            // We only care about request to RedirectUri endpoint.
+            while (context.Request.Url.AbsolutePath != RedirectUri.AbsolutePath)
+            {
+                context = await http.GetContextAsync();
+            }
+
+            context.Response.ContentType = "text/html";
+
+            // Respond with a page which runs JS and sends URL fragment as query string
+            // to TokenRedirectUri.
+            using (var file = File.OpenRead("index.html"))
+            {
+                file.CopyTo(context.Response.OutputStream);
+            }
+
+            context.Response.OutputStream.Close();
+        }
+
+        private async Task<OAuth2Response> HandleJSRedirect(HttpListener http)
+        {
+            var context = await http.GetContextAsync();
+
+            // We only care about request to TokenRedirectUri endpoint.
+            while (context.Request.Url.AbsolutePath != JSRedirectUri.AbsolutePath)
+            {
+                context = await http.GetContextAsync();
+            }
+
+            var redirectUri = new Uri(context.Request.QueryString["url_with_fragment"]);
+
+            var result = DropboxOAuth2Helper.ParseTokenFragment(redirectUri);
+
+            return result;
+        }
+
+        private async Task<string> GetAccessToken()
+        {
+            var accessToken = string.Empty;
+            try
+            {
+                Console.WriteLine("Waiting for credentials.");
+                var state = Guid.NewGuid().ToString("N");
+                var authorizeUri =
+                    DropboxOAuth2Helper.GetAuthorizeUri(OAuthResponseType.Token, ApiKey, RedirectUri, state: state);
+                var http = new HttpListener();
+                http.Prefixes.Add(LoopbackHost);
+
+                http.Start();
+
+                System.Diagnostics.Process.Start(authorizeUri.ToString());
+
+                // Handle OAuth redirect and send URL fragment to local server using JS.
+                await HandleOAuth2Redirect(http);
+
+                // Handle redirect from JS and process OAuth response.
+                var result = await HandleJSRedirect(http);
+
+                if (result.State != state)
+                {
+                    // The state in the response doesn't match the state in the request.
+                    return null;
+                }
+
+                Console.WriteLine("and back...");
+
+                // Bring console window to the front.
+                SetForegroundWindow(GetConsoleWindow());
+
+                accessToken = result.AccessToken;
+                var uid = result.Uid;
+                Console.WriteLine("Uid: {0}", uid);
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error: {0}", e.Message);
+                return null;
+            }
+
+            return accessToken;
+        }
+    }
+}
---
+++ Projects/GoogleDriveTest/Program.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using Google.Apis.Auth.OAuth2;
+using Google.Apis.Download;
+using Google.Apis.Drive.v3;
+using Google.Apis.Services;
+using Google.Apis.Util.Store;
+using File = Google.Apis.Drive.v3.Data.File;
+
+namespace GoogleDriveTest
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            string[] scopes =
+            {
+                DriveService.Scope.DriveFile,
+                DriveService.Scope.Drive,
+                DriveService.Scope.DriveAppdata
+            };
+            var clientId =
+                "612213896674-6s8pr9d81vs9js0964tsetap7qjf189i.apps.googleusercontent.com"; // From https://console.developers.google.com
+            var clientSecret = "4TyuYuWszueyEWFlWU-gqYEQ"; // From https://console.developers.google.com
+
+            // here is where we Request the user to give us access, or use the Refresh Token that was previously stored in %AppData%
+            GoogleWebAuthorizationBroker.Folder = "Drive.Sample";
+            var credential = GoogleWebAuthorizationBroker.AuthorizeAsync(new ClientSecrets
+                {
+                    ClientId = clientId,
+                    ClientSecret = clientSecret
+                },
+                scopes,
+                "Admin",
+                CancellationToken.None,
+                new FileDataStore("Daimto.GoogleDrive.Auth.Store")).Result;
+
+            var service = new DriveService(new BaseClientService.Initializer
+            {
+                HttpClientInitializer = credential,
+                ApplicationName = "Drive API Sample"
+            });
+
+            
+            var filelist = retrieveAllFiles(service,"mimeType = 'application/vnd.google-apps.folder' and name = 'TaskWorkspace'");
+            string folderId = string.Empty;
+            if (!filelist.Any())
+            {
+                folderId = CreateFolderAndReturnId(service, "TaskWorkspace");
+            }
+            else
+            {
+                folderId = filelist.First().Id;
+            }
+
+            if (!string.IsNullOrEmpty(folderId))
+            {
+                //remove
+                var removeLIst = retrieveAllFiles(service,$"name = 'WorkspaceManager.vsix.zip'");
+                foreach (var fileRemove in removeLIst)
+                {
+	                service.Files.Delete(fileRemove.Id).Execute();
+                }
+
+                // upload
+                var fileMetadata = new File()
+                {
+                    Name = "WorkspaceManager.vsix.zip",
+                    Parents = new List<string>() { folderId}
+                };
+                FilesResource.CreateMediaUpload request;
+                using (var stream = new System.IO.FileStream("C:/Users/Yariki/Downloads/WorkspaceManager.vsix.zip",
+                                        System.IO.FileMode.Open))
+                {
+                    request = service.Files.Create(
+                        fileMetadata, stream,"application/zip");
+                    request.Fields = "id";
+                    
+                    request.Upload();
+                }
+                var file = request.ResponseBody;
+                Console.WriteLine("File ID: " + file.Id);
+
+                var fileList = retrieveAllFiles(service, $"name = 'WorkspaceManager.vsix.zip'");
+                
+                //download
+
+                var fileRequested = fileList.First().WebContentLink;
+                
+                MediaDownloader downloader = new MediaDownloader(service);
+                downloader.ProgressChanged += progress => { Console.WriteLine(progress.Exception?.ToString()); };
+                downloader.Download(file.WebContentLink,
+	                System.IO.File.Create("c:/temp/WorkspaceManager.vsix.zip"));
+
+                
+
+
+            }
+            
+            
+            foreach (var file in filelist) Console.WriteLine( $"{file.Name} - {file.MimeType} - {file.Id}");
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey();
+        }
+
+        private static string CreateFolderAndReturnId(DriveService service, string name)
+        {
+            File fileMetadata = new File();
+            fileMetadata.Name = "TaskWorkspace";
+            fileMetadata.MimeType = "application/vnd.google-apps.folder";
+
+            File file = service.Files.Create(fileMetadata).Execute();
+            Console.WriteLine("Folder ID: " + file.Id);
+            return file.Id;
+        }
+        
+        private static void UploadFile(DriveService service)
+        {
+            
+        }
+
+        public static List<File> retrieveAllFiles(DriveService service,string searchQuery)
+        {
+            var result = new List<File>();
+            var request = service.Files.List();
+            //request.Fields = "id, webContentLink";
+            request.Q = searchQuery;
+
+            do
+            {
+                try
+                {
+                    var files = request.Execute();
+
+                    result.AddRange(files.Files);
+                    request.PageToken = files.NextPageToken;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("An error occurred: " + e.Message);
+                    request.PageToken = null;
+                }
+            } while (!string.IsNullOrEmpty(request.PageToken));
+
+            return result;
+        }
+    }
+}
