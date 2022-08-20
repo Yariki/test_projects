3b991c08499276f72395fd57fcdf745a32aa0338
Yaroslav Lisovskyi <iyariki.ya@gmail.com>
2022-08-20 09:25:07 +0300

    test Monitor Pulse and Wait

---
+++ monitortest/monitorthread/Program.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Concurrent;
+
+internal class Program
+{
+        static object lockObject = new();
+        static ConcurrentQueue<string> queue = new();
+
+    private static void Main(string[] args)
+    {
+        CancellationTokenSource cancellationTokenSource = new();
+        
+        var producer = new Thread(Produce);
+        var consumer = new Thread(Consume);
+        producer.Start(cancellationTokenSource.Token);
+        consumer.Start(cancellationTokenSource.Token);
+
+        producer.Join();
+        cancellationTokenSource.Cancel();
+        lock(lockObject)
+            Monitor.Pulse(lockObject);
+        consumer.Join();
+    }
+
+    private static void Produce(object state)
+    {
+        CancellationToken token = (CancellationToken)state;
+
+        for(int i = 1; i < 20; i++)
+        {
+            lock(lockObject)
+            {
+                queue.Enqueue($"Parameter {i}");
+                Monitor.Pulse(lockObject);
+                Thread.Sleep(250);
+            }
+        }
+        Console.WriteLine("Exit Prodcucer...");
+    }
+
+    private static void Consume(object state)
+    {
+        CancellationToken token = (CancellationToken)state;
+
+        while(!token.IsCancellationRequested)
+        {
+            lock(lockObject)
+            {
+                Monitor.Wait(lockObject);
+                queue.TryDequeue(out var str);
+
+                Console.WriteLine(str);
+            }        
+        }
+        Console.WriteLine("Exit COnsumer...");
+    }
+}
